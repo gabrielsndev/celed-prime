7ac92b52070ebd05bed12b4d5e8d40f437811f02
gabrielsdev <gabrielsndev@gmail.com>
2026-01-20 13:48:39 -0300

    spring inicialz e config.properties

---
+++ backend/src/main/java/com/celedprime/api/CeledPrimeApplication.java
@@ -0,0 +1,13 @@
+package com.celedprime.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeledPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeledPrimeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/celedprime/api/CeledPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celedprime.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeledPrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
