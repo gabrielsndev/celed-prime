3c73eed51d38b2c025f47745ffaeaf3c928fce80
gabrielsdev <gabrielsndev@gmail.com>
2026-03-07 14:29:53 -0300

    Iniciando component de home

--- frontend/celed-prime-web/src/app/app.ts
+++ frontend/celed-prime-web/src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'cp-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
