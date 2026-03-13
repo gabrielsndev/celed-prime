bb878808e8bfe94cb1f26eac46baa55958a58d09
gabrielsdev <gabrielsndev@gmail.com>
2026-03-13 16:05:11 -0300

    perto de finalizar o calendário

--- frontend/celed-prime-web/src/app/shared/components/reserve-calendar/reserve-calendar.component.ts
+++ frontend/celed-prime-web/src/app/shared/components/reserve-calendar/reserve-calendar.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 @Component({
   selector: 'cp-reserve-calendar',
@@ -11,12 +13,30 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     CommonModule,
     MatCardModule,
     MatDatepickerModule,
-    MatNativeDateModule,
-    MatButtonModule
+    MatButtonModule,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+    provideNativeDateAdapter()
   ],
   templateUrl: './reserve-calendar.component.html',
   styleUrl: './reserve-calendar.component.scss',
 })
 export class ReserveCalendarComponent {
 
+  dates:string[] = ['2026-03-15', '2026-03-17', '2026-03-18', '2026-03-21']
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate:Date, view:string) => {
+    if (view === 'month') {
+    
+      const year = cellDate.getFullYear();
+      const month = (cellDate.getMonth() + 1).toString().padStart(2, '0');
+      const day = cellDate.getDate().toString().padStart(2, '0');
+      const formattedDate = `${year}-${month}-${day}`;
+
+      return this.dates.includes(formattedDate) ? 'special-date' : '';
+    }
+
+    return '';
+  };
 }
